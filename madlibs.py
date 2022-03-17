7474c57cd7fd4a77398090133cf4cf57a4de932b
RangerDevv <meerabdul2008@gmail.com>
2022-03-17 22:01:53 +0000

    basic structure

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjetive:")
+noun1 = input('Noun:')
+noun2 = input('Another Noun:')
+letsplay = input('Do you want to play? type yes or no (NO CAPS!)')
+
+if letsplay == 'yes':
+    print('I', adj)
+elif letsplay == 'no':
+    print('please try I bed you!')
