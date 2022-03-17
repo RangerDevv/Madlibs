9b14097ecf5fc2fa2027618b5b8e0b94d535c4a6
RangerDevv <meerabdul2008@gmail.com>
2022-03-17 23:43:20 +0000

    first madlibsgit add .

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,10 @@
 adj = input("Adjetive:")
 noun1 = input('Noun:')
 noun2 = input('Another Noun:')
+verb =  input('Verb:')
 letsplay = input('Do you want to play? type yes or no (NO CAPS!)')
 
 if letsplay == 'yes':
-    print('I', adj)
+    print('I', adj , 'programming that I can' , noun1 , 'for it. It makes be feel' , adj , 'and helps me' , verb , 'faster. it helps me relieve my stress and makes me' , adj , 'So, weather what you think about programming... I ' , adj , 'IT!!')
 elif letsplay == 'no':
-    print('please try I bed you!')
+    print('please try I beg you!')
